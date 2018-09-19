99b8b960b2aa5d0d378ffa0ebfa1d1128dc828b9
Solee Lee <carolinesoleelee@gmail.com>
2018-09-19 22:13:50 +0000

    automatically backed up by learn

--- spec/parrot_spec.rb
+++ spec/parrot_spec.rb
@@ -14,7 +14,7 @@ describe '#parrot' do
 
   def parrot(says = "Squawk!")
     puts "#{says}"
-    "#{{says}}"
+    "#{says}"
   end
 
   it 'should output the given phrase when called with an argument' do
